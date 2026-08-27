3227054c7a3d99470542269e845b7477d13d71fe
Sven Sieverding <sven.sieverding@hec.de>
2026-08-27 17:27:36 +0200

    played arround with define

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import App from './App'
 import './index.css'
 
 
-console.log(document.getElementById('root'))
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name][extname]',
-      },
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  return {
+    define: {
+      // Inject a constant that you can use in your app
+      __API_BASE__: JSON.stringify(
+        isDev ? 'http://localhost:3000/api' : 'https://api.example.com'
+      ),
+      __LOCAL_SERVE__: JSON.stringify(isDev) // true in dev, false in prod
     },
-    assetsInlineLimit: 1024 * 1024, // Inline alle Assets bis 1MB
-    minify: 'terser',
-  },
+    plugins: [react()],
+    server: {
+      port: 5173,
+      open: true,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name].js',
+          assetFileNames: '[name][extname]',
+        },
+      },
+      assetsInlineLimit: 1024 * 1024, // Inline alle Assets bis 1MB
+      minify: 'terser',
+    }
+  }
 })
